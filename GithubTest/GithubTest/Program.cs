4118e8925ec1e95454ba8435a16ed6672ac0f5bc
재우 송 <saw145@naver.com>
2020-10-11 21:16:57 +0900

    TestGithub

---
+++ GithubTest/GithubTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "123" + "456";
+            Console.WriteLine(result);
+
+            result = "Hello" + " " + "World!";
+            Console.WriteLine(result);
+        }
+    }
+}
